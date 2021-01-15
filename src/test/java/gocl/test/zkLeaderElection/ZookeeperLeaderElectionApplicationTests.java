465c0d949993ced06c8f4c6d34aaa3cfa739ec1c
gerardo cerqueda <gercerqueda@gmail.com>
2021-01-15 13:35:28 -0600

    Removed spring web, Added uniqueLeader Test

--- src/test/java/gocl/test/zkLeaderElection/ZookeeperLeaderElectionApplicationTests.java
+++ src/test/java/gocl/test/zkLeaderElection/ZookeeperLeaderElectionApplicationTests.java
@@ -1,13 +1,32 @@
 package gocl.test.zkLeaderElection;
 
+import gocl.test.zkLeaderElection.zookeeper.candidate.CurrentLeadership;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ZookeeperLeaderElectionApplicationTests {
 
+	@Autowired
+	private CurrentLeadership leadOne;
+
 	@Test
-	void contextLoads() {
+	public void uniqueLeaderTest() throws Exception {
+		CurrentLeadership leadTwo = createInstance();
+		CurrentLeadership leadThree = createInstance();
+
+		// validate only one leader
+		Assertions.assertTrue(leadOne.isLeader() ^ leadTwo.isLeader() ^ leadThree.isLeader(),
+			"There is more than one leader");
+	}
+
+	private CurrentLeadership createInstance() {
+		SpringApplicationBuilder instance = new SpringApplicationBuilder(ZookeeperLeaderElectionApplication.class);
+		instance.run();
+		return instance.context().getBean(CurrentLeadership.class);
 	}
 
 }
