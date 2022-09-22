8d6006f40cadbcaa50a7c06bf91d1d2777f74f92
William Polinchak <william.polinchak@windstream.com>
2022-09-22 10:37:17 -0400

    Adding a Junit test.

---
+++ src/test/java/gocl/test/zkLeaderElection/ZookeeperLeaderElectionApplicationTests.java
@@ -0,0 +1,39 @@
+package gocl.test.zkLeaderElection;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.apache.curator.test.TestingServer;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+public class ZookeeperLeaderElectionApplicationTests {
+
+    @Test
+	public void testMethod() throws Exception {
+
+		int zkPort = 9030;
+		TestingServer server = new TestingServer(zkPort);
+
+		int port = 9031;
+
+		ConfigurableApplicationContext context = new SpringApplicationBuilder(
+				ZookeeperLeaderElectionApplication.class).run("--server.port=" + port,
+						"--spring.config.use-legacy-processing=true",
+						"--management.endpoints.web.exposure.include=*",
+						"--spring.cloud.zookeeper.connect-string=localhost:" + zkPort);
+
+		ResponseEntity<String> response = new TestRestTemplate()
+				.getForEntity("http://localhost:" + port + "/", String.class);
+		assertEquals(response.getStatusCode(), HttpStatus.OK);
+
+		context.close();
+		server.close();
+	}
+
+
+
+}
