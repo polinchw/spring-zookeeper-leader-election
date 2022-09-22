ea46adde9b557c6b321677a40f25f5d0f2ee38ad
William Polinchak <william.polinchak@windstream.com>
2022-09-22 08:36:03 -0400

    Add rest api.

---
+++ src/main/java/gocl/test/zkLeaderElection/rest/Zooleadership.java
@@ -0,0 +1,26 @@
+package gocl.test.zkLeaderElection.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import gocl.test.zkLeaderElection.zookeeper.candidate.CurrentLeadership;
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@Slf4j
+public class Zooleadership {
+
+    @Autowired
+    private CurrentLeadership currentLeadership;
+
+    @RequestMapping("/")
+    public String leader() {
+        log.info("leader invoked");
+        if (this.currentLeadership.isLeader()) {
+            return "yes";
+        }
+        return "no";
+    }
+    
+}
